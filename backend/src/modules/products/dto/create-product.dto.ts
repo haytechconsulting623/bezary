0b070eb7bb1c6a5f92526c2362f401e651d20b7e
mandroro <yvesaimable42@gmail.com>
2025-12-24 10:52:30 +0300

    feat(module):Update of the product module:entity, controller, service, repository, create-product-dto, add update-product-dto and add relation with vendor entity

--- backend/src/modules/products/dto/create-product.dto.ts
+++ backend/src/modules/products/dto/create-product.dto.ts
@@ -1,1 +1,22 @@
-export class CreateProductDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { ProductStatus } from '../products.entity';
+import { IsEnum, IsOptional, IsString } from '@nestjs/class-validator';
+
+export class CreateProductDto {
+  @ApiProperty({ example: 'Chaussures de sport' })
+  @IsString()
+  nameProduct: string;
+
+  @ApiProperty({ example: 'Sport' })
+  @IsString()
+  categoryProduct: string;
+
+  @ApiProperty({ enum: ProductStatus, example: ProductStatus.DRAFT, required: false })
+  @IsOptional()
+  @IsEnum(ProductStatus)
+  statusProduct?: ProductStatus;
+
+  @ApiProperty({ default: false, required: false })
+  @IsOptional()
+  isDelete?: boolean;
+}
--- backend/src/modules/products/products.module.ts
+++ backend/src/modules/products/products.module.ts
@@ -1,1 +1,14 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ProductsController } from './products.controller';
+import { ProductsService } from './products.service';
+import { ProductsEntity } from './products.entity';
+import { ProductsRepository } from './products.repository';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([ProductsEntity])],
+  controllers: [ProductsController],
+  providers: [ProductsService, ProductsRepository],
+  exports: [ProductsService, ProductsRepository],
+})
 export class ProductsModule {}
