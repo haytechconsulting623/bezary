5aed697e5cd78d1e61225898e1ed90f440026956
mandroro <yvesaimable42@gmail.com>
2025-12-24 07:55:39 +0300

    feat(config):separated configuration database in other file and modify file App.module.ts

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,19 +1,15 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 import { UsersModule } from './modules/users/users.module';
+import { databaseConfig } from './config/database.config';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: '0000',
-      database: 'bezarydb',
-      autoLoadEntities: true,
-      synchronize: true,
+    ConfigModule.forRoot({
+      isGlobal: true,
     }),
+    TypeOrmModule.forRootAsync(databaseConfig),
     UsersModule,
   ],
 })
